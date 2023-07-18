f467bb7af853654d1af7144deda0014cd2b1047e
Mauricio Gallardo <xmagcx@gmail.com>
2023-07-18 15:36:36 -0400

    add readme and licence

---
+++ README.md
@@ -0,0 +1,73 @@
+
+===
+## Papar Information
+- Title:  `questions and answers`
+
+
+## Install & Dependence
+- python 3.10
+- Docker
+
+
+## Use
+- using python
+  ```
+  python3 pip install -r requirements.txt
+  python3 -m uvicorn app:app --host 0.0.0.0 --port 8080 --reload --timeout-keep-alive 600 --workers 10
+  ```
+- using docker
+  ```
+  docker build -t lmqg/app:latest .
+  docker run -p 8080:8080 lmqg/app:latest
+  ```
+## Execute
+
+- using python
+  ```
+  import requests
+  import json
+  
+
+  def api(url,value):
+    
+      headers = {"Content-Type": "application/json; charset=utf-8"}
+      
+      data = {
+          "context": value
+      }
+      
+      response = requests.post(url, headers=headers, json=data)
+      
+      #print("Status Code", response.status_code)
+      #print("JSON Response ", response.json())
+      return response.json()
+  
+  if __name__ == "__main__":
+    url="http://127.0.0.1:8080/generation"
+    context="""William Turner was an English painter who specialised in watercolour landscapes. He is often known as William Turner of Oxford or just Turner of Oxford to distinguish him from his contemporary, J. M. W. Turner. Many of Turner's paintings depicted the countryside around Oxford. One of his best known pictures is a view of the city of Oxford from Hinksey Hill."""
+    response = api(url,context)
+    print(response)
+
+  ```
+
+## Directory Hierarchy
+```
+|—— .gitignore
+|—— Dockerfile
+|—— LICENCE
+|—— __pycache__
+|    |—— app.cpython-310.pyc
+|—— app.py
+|—— execute
+|—— note.py
+|—— readme.MD
+|—— requirements.txt
+```
+
+## References
+- [code-1](https://github.com/asahi417/lm-question-generation)
+- [code-2](https://huggingface.co/lmqg)
+  
+## License
+
+- MIT
--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from lmqg import TransformersQG
-#import es_core_news_sm
 import en_core_web_sm
-#import time
-#start_time = time.time()
 
 
 class Item(BaseModel):
@@ -34,8 +31,6 @@ def generation(data: Item):
             model="lmqg/t5-small-squad-qg-ae"
             language="en"
             model = TransformersQG(language=language, model=model)
-            #context = "William Turner was an English painter who specialised in watercolour landscapes. He is often known as William Turner of Oxford or just Turner of Oxford to distinguish him from his contemporary, J. M. W. Turner. Many of Turner's paintings depicted the countryside around Oxford. One of his best known pictures is a view of the city of Oxford from Hinksey Hill."
-            #context ="THIS IS A LEGEND told and retold among the people of Argentina about powerful friendship between a girl and the puma.Five hundred years ago when the Spanish entered South America, Native American tribes often fought back against the invaders. One way tribes could put pressure on the Spanish was to surround their settlements. This is what happened in the early 1500's when Maldonado, a Spanish girl, was 15 years old.Hostile Native Americans of the Querandí tribe had encircled the Spanish settlement where Maldonado lived."
             question_answer = model.generate_qa(data.context)
         except Exception as e:
             error = str(e)
@@ -48,14 +43,3 @@ def generation(data: Item):
     return question_answer
 
 
-
-
-#print("--- %s seconds ---" % (time.time() - start_time))
-##nlp = es_core_news_sm.load()
-#
-#model = TransformersQG(language='es', model='lmqg/t5-large-squad-qg-ae')
-#context = """
-#Un asno y un caballo vivían juntos desde su más tierna infancia y, como buenos amigos que eran, utilizaban el mismo establo, compartían la bandeja de heno, y se repartían el trabajo equitativamente. Su dueño era molinero, así que su tarea diaria consistía en transportar la harina de trigo desde el campo al mercado principal de la ciudad.La rutina era la misma todas las mañanas: el hombre colocaba un enorme y pesado saco sobre el lomo del asno, y minutos después, otro igual de enorme y pesado sobre su lomo
-#"""
-#question_answer = model.generate_qa(context)
-#print(question_answer)
